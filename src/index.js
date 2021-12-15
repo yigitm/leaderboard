d411a5f605fa82270b5237446c653e3b3842bc8a
Yigit Mersin <yigitmersin@gmail.com>
2021-12-15 17:52:21 +0300

    Remove form tag

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './style.css';
+
+const getUsers = () => {
+  fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2fUdz/scores',
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      //document.getElementsByTagName('ul')[0].innerHTML = `<li></li>`;
+      console.log(data);
+    });
+};
+
+getUsers();
